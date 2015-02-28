fa982fd3cd55322b17216f9198a91f65b160a958
Dan Hoang <danhoang@Dans-MacBook-Air.local>
2015-02-28 13:36:43 -0800

    Initial Commit

---
+++ HomelessHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomelessHelper
+//
+//  Created by Dan Hoang on 2/28/15.
+//  Copyright (c) 2015 Dan Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HomelessHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomelessHelper
+//
+//  Created by Dan Hoang on 2/28/15.
+//  Copyright (c) 2015 Dan Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
